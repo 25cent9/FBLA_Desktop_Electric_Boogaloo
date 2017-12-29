92e1708a58d376aaf9b5031773f1f3e52757e7ad
Innocent Niyibizi <25cent9@gmail.com>
2017-12-29 01:38:44 -0600

    Roadblick: Welp, inseting with knex and SQLite3 is quite a hassle

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const { app, BrowserWindow, ipcMain, dialog } = require("electron")
 var knex = require("knex")({
     client: "sqlite3",
     connection: {
-        filename: "./student.db"
-    }
+        filename: "./test.db"
+    },
+    useNullAsDefault: true
 });
 
 app.on("ready", () => {
@@ -49,6 +50,45 @@ app.on("ready", () => {
         result.then(function(rows) {
             mainWindow.webContents.send("hereAreTheSchools", rows);
         });
+    });
+
+    ipcMain.on("insertMember", (event, member) => {
+        let test = knex("Student").insert([
+            {memberID: member.memberID},
+            {Fname: member.Fname},
+            {Lname: member.Lname},
+            {Gender: member.Gender},
+            {Email: member.Email}
+        ]);
+        // var studentInset = "INSERT INTO Student (memberID, Fname, Lname, Gender, Email) Values ("+member.memberID+", \'"+member.Fname+"\', \'"+member.Lname+"\', \'"+member.Gender+"\', \'"+member.Email+"\' )";
+
+        // var schoolInsert = "INSERT INTO School (name, State) Values ( \'"+member.schoolName+"\', \'"+member.state+"\' )";
+
+        // var attendsInsert = "INSERT INTO Attends (memberID, school_name, grade_level) Values ("+member.memberID+", \'"+member.schoolName+"\', "+member.gradeLevel+")";
+
+        // var memberInsert = "INSERT INTO Membership (memberID, year_joined, active, amount_owed) Values ("+member.memberID+", "+member.year+", "+member.active+", "+member.amountDue+" )";
+
+        try{
+            /*
+            console.log(studentInset+"\n"+schoolInsert+"\n"+attendsInsert+" \n"+memberInsert);
+            let studentResult = knex.raw(studentInset);
+            let schoolResult = knex.raw(schoolInsert);
+            let attendsResult = knex.raw(attendsInsert)
+            let memberResult = knex.raw(memberInsert);
+
+            console.log(studentResult+"\n"+schoolResult+"\n"+attendsResult+"\n"+memberResult)
+            */
+            test.then(function(rows) {
+                mainWindow.webContents.send("allClear", rows);
+            });
+            console.log(test);
+            
+        }
+        catch(err){
+            dialog.showErrorBox("Insertion Error","There was an error with inserting");
+            mainWindow.webContents.send("whoops", "You goofed, my dude");
+        }
+
     });
 });
 
