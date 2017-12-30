cda241a7a1c91c12b5ab851c60b66e44dcc2d229
Innocent Niyibizi <25cent9@gmail.com>
2017-12-30 00:19:08 -0600

    Finally: It's time for a release. :upside_down_face:

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Future Business Leaders of America - Desktop Application (Eletric Boogaloo)
 
-This project is soley to revamp my [original](https://github.com/25cent9/FBLA-Desktop-Application-Programming) FBLA desktop project that I wrote in `VB.NET`. I really wanted to get some experience with Node.js and Electron so here we are :upside_down_face: 
+This project is soley to revamp my [original](https://github.com/25cent9/FBLA-Desktop-Application-Programming) FBLA desktop project that I wrote in `VB.NET`. I really wanted to get some experience with Node.js, Electron, and write my application with real SQL and proper database modeling so here we are :upside_down_face: 
+
+## CRUD:
+- [x] Creation
+- [x] Reading 
+- [ ] Updating
+- [ ] Deletion
--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// var electron = require("electron");
-// var app = electron.app;
-// var BrowserWindow = electron.BrowserWindow;
-// const ipcMain = require("electron");
-
 const { app, BrowserWindow, ipcMain, dialog } = require("electron")
 
 var knex = require("knex")({
     client: "sqlite3",
     connection: {
-        filename: "./test.db"
+        filename: "./student.db"
     },
     useNullAsDefault: true
 });
@@ -19,16 +14,19 @@ app.on("ready", () => {
         width: 900,
         minHeight: 400,
         minWidth: 900, 
-        show: false})
+        show: false
+    });
     mainWindow.setTitle("FBLA Electric Boogaloo")
     mainWindow.loadURL(`file://${__dirname}/main.html`) 
     mainWindow.once("ready-to-show", () => { mainWindow.show() })
-
+    
+    //Displaying the error message in a error box
     ipcMain.on("anError", (event, args) => {
         dialog.showErrorBox("An error", args);
 
     });
 
+    //Getting all the members in the database
     ipcMain.on("showAll", (event, args) => {
         let result = knex.raw("SELECT * FROM Student as S, Attends as A WHERE S.memberID = A.memberID ORDER BY S.Lname");
         result.then(function(rows){
@@ -36,14 +34,16 @@ app.on("ready", () => {
         });
     });
 
+    //Getting all the members for a specified grade level
     ipcMain.on("updateMembers", (event, args) => {
-        var querey = "SELECT * FROM Student as S, Attends as A WHERE S.memberID = A.memberID AND A.grade_level = " + args;
+        var querey = "SELECT * FROM Student as S, Attends as A WHERE S.memberID = A.memberID AND A.grade_level = " + args+" ORDER BY S.Lname";
         let result = knex.raw(querey);
         result.then(function(rows) {
             mainWindow.webContents.send("gotNewMembers", rows);
         });
     });
 
+    //Getting all the schools currently in the database
     ipcMain.on("getMeTheSchools", (event, args) => {
         var querey = "SELECT * FROM School";
         let result = knex.raw(querey);
@@ -52,6 +52,7 @@ app.on("ready", () => {
         });
     });
 
+    //Inserting into the Student table
     ipcMain.on("insertStudent", (event, member) => {
         var studentInset = "INSERT INTO Student (memberID, Fname, Lname, Gender, Email) Values ("+member.memberID+", \'"+member.Fname+"\', \'"+member.Lname+"\', \'"+member.Gender+"\', \'"+member.Email+"\' )";
 
@@ -69,6 +70,7 @@ app.on("ready", () => {
         }
     });
 
+    //Inserting into the School table
     ipcMain.on("insertSchool", (event, member) => {
         var schoolInsert = "INSERT INTO School (name, State) Values ( \'"+member.schoolName+"\', \'"+member.state+"\' )";
         try{
@@ -83,6 +85,7 @@ app.on("ready", () => {
         }
     });
 
+    //Inseting into the Attends table
     ipcMain.on("insertAttends", (event, member) => {
         var attendsInsert = "INSERT INTO Attends (memberID, school_name, grade_level) Values ("+member.memberID+", \'"+member.schoolName+"\', "+member.gradeLevel+")";
         try{
@@ -97,6 +100,7 @@ app.on("ready", () => {
         }
     });
 
+    //Inserting into the member table
     ipcMain.on("insertMember", (event, member) => {
         var memberInsert = "INSERT INTO Membership (memberID, year_joined, active, amount_owed) Values ("+member.memberID+", "+member.year+", "+member.active+", "+member.amountDue+" )";
         try{
@@ -111,10 +115,12 @@ app.on("ready", () => {
         }
     });
 
-    ipcMain.on("weGucci", (event, args) => {
-        dialog.showMessageBox({ message: "Member has been added", 
+    //Displaying a message box upon all checks passing and member being inserted properly
+    ipcMain.on("weGucci", (event, member) => {
+        dialog.showMessageBox({ message: member.Fname+" "+member.Lname+" has been added!", 
                                 buttons: ["OK"] });
     });
+
 });
 
 app.on("window-all-closed", () => { app.quit() })
